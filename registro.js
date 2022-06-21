067ba66742378636af894f8f2c066679427e0303
ispipa <ispipa123@gmail.com>
2022-06-21 13:32:02 +0200

    agregar datos

--- registro.js
+++ registro.js
@@ -2,10 +2,21 @@
 var codigos_usuarios = [];
 var fechas_usuarios=[];
 var horas_usuarios=[];
+if(localStorage.getItem("codigos_usuarios") == null)
+{
+    codigos_usuarios = [];
+    fechas_usuarios=[];
+    horas_usuarios=[];
+}
+else
+{
+    codigos_usuarios =JSON.parse(localStorage.getItem("codigos_usuarios"));
+    fechas_usuarios=JSON.parse(localStorage.getItem("fechas_usuarios"));
+    horas_usuarios=JSON.parse(localStorage.getItem("horas_usuarios"));
+}
 
-//comprobar y guardar la informacion
-
-function guardarComprobar()
+//comprobar la informacion
+function comprobarDatos()
 {
     const codigo_usario = document.querySelector("#codigo_usuario");
     var codigo = codigo_usario.value;
@@ -15,15 +26,18 @@ function guardarComprobar()
             var fechaHoy = new Date();
             var fechaAct = fechaHoy.getDate() + "/" + (fechaHoy.getMonth()+1) + "/" + fechaHoy.getFullYear();
             var hora = fechaHoy.getHours() + ':' + fechaHoy.getMinutes();
-        // guardamos la fecha y la hora
+            // guardamos la fecha y la hora
             codigos_usuarios.push(codigo);
             fechas_usuarios.push(fechaAct);
             horas_usuarios.push(hora);
-            console.log(codigos_usuarios);
-            console.log(fechas_usuarios);
-            console.log(horas_usuarios);
-            localStorage.setItem("codigos_usuarios",codigos_usuarios);
-            localStorage.setItem("fechas_usuarios",fechas_usuarios);
-            localStorage.setItem("horas_usuarios",horas_usuarios);
+            guardarDatos();
     }
+}
+//GUARDAR LOS DATOS
+function guardarDatos()
+{
+    //guarda DATOSS USUARIO
+    localStorage.setItem("codigos_usuarios",JSON.stringify(codigos_usuarios));
+    localStorage.setItem("fechas_usuarios",JSON.stringify(fechas_usuarios));
+    localStorage.setItem("horas_usuarios",JSON.stringify(horas_usuarios));
 }
