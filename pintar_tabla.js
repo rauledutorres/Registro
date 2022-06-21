29b3d0f43f3e9c07a0623cb81054d2ab63259d63
ispipa <ispipa123@gmail.com>
2022-06-21 14:24:43 +0200

    cambios

--- pintar_tabla.js
+++ pintar_tabla.js
@@ -2,6 +2,9 @@
 var codigos_usuarios = [];
 var fechas_usuarios=[];
 var horas_usuarios=[];
+
+var fechaHoy = new Date();
+
 if(localStorage.getItem("codigos_usuarios") == null)
 {
     codigos_usuarios = [];
@@ -19,7 +22,8 @@ else
 function pintar()
 {
     const pint=document.querySelector("#table2");
-    console.log(pint)
+    var ultimaDato = horas_usuarios.length -1;
+    console.log(ultimaDato)
     for (let i = 0; i< codigos_usuarios.length; i++) {
         var row=document.createElement("tr");
         var cod=document.createElement("td");
@@ -27,10 +31,18 @@ function pintar()
         var hor=document.createElement("td");
         cod.textContent=codigos_usuarios[i];
         fech.textContent=fechas_usuarios[i];
+        if(horas_usuarios.length -1)
+        {
+            hor.setAttribute("id","hora");
+        }
         hor.textContent=horas_usuarios[i];
         pint.appendChild(row);
         row.appendChild(cod);
         row.appendChild(fech);
         row.appendChild(hor);
     }
+}
+function pintarHora()
+{
+
 }
