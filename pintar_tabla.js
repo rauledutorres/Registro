6881d86e0bcab7c6774938fe1b69eeaf335f5de6
KevsCha <k-e-v-i-n_13_@hotmail.com>
2022-06-22 14:27:55 +0200

    ahah

--- pintar_tabla.js
+++ pintar_tabla.js
@@ -85,4 +85,7 @@ function visible(){
     }
 }
 btn_register.addEventListener('click',visible);
-
+///////////////////////////////////////////////////////////////////////////////////
+function salirDato(){
+    window.location.assign("http://127.0.0.1:5500/index.html?");
+}
