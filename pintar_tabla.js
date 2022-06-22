12434458a7df67fc4241030261bb0082b86b5067
rauledutorres <rauleduardotorres98@gmail.com>
2022-06-22 11:03:38 +0200

    creacion de carta

--- pintar_tabla.js
+++ pintar_tabla.js
@@ -11,9 +11,31 @@ if(localStorage.getItem("usuarios") == null)
 else
 {
     usuarios =JSON.parse(localStorage.getItem("usuarios"));
-
+    pintarCarta();
     pintar();
 }
+
+function pintarCarta(){
+    var cuerpoCarta = document.querySelector(".card-body");
+    var registroUsuario = JSON.parse(localStorage.getItem("resgistro_usuarios"));
+    var codigo_usuario = JSON.parse(localStorage.getItem("codigo_usuario"));
+    console.log(codigo_usuario);
+    for (let i = 0; i < registroUsuario.length; i++) {
+        var h5 = document.createElement("h5");
+        var h6 = document.createElement("h6");
+        if(registroUsuario[i].codigo === codigo_usuario )
+        {
+            h5.innerText = registroUsuario[i].nombre;
+            h6.innerText = registroUsuario[i].codigo;
+        }
+    cuerpoCarta.appendChild(h5);
+    cuerpoCarta.appendChild(h6);
+            
+    }
+
+
+}
+
 
 function pintar()
 {
@@ -26,7 +48,7 @@ function pintar()
         var cod=document.createElement("td");
         var fech=document.createElement("td");
         var hor=document.createElement("td");
-            if(usuarios[i].codigo === codigo_usuario )
+        if(usuarios[i].codigo === codigo_usuario )
         {
             cod.textContent=usuarios[i].codigo;
             fech.textContent=usuarios[i].objeto_fecha.fechaAct;
